aab9758c5774495f57f9248206a35b84d6b5f185
Fedka <fedkaevgen@gmail.com>
2019-05-11 20:22:47 +0300

    No Promotions

--- upload/admin/controller/extension/extension/promotion.php
+++ upload/admin/controller/extension/extension/promotion.php
@@ -1,6 +1,7 @@
 <?php
 class ControllerExtensionExtensionPromotion extends Controller {
 	public function index() {
+		/*
 		$curl = curl_init();
 
 		curl_setopt($curl, CURLOPT_URL, OPENCART_SERVER . 'index.php?route=api/promotion&type=' . substr($this->request->get['route'], strrpos($this->request->get['route'], '/') + 1));
@@ -19,5 +20,8 @@ class ControllerExtensionExtensionPromotion extends Controller {
 		} else {
 			return '';
 		}
+		*/
+		
+		return '';
 	}
 }
